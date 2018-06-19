24e43374271b31648b430e30150d688d518d63c9
yuanusa <39840213+yuanusa@users.noreply.github.com>
2018-06-18 20:01:10 -0400

    Update NYC.flights.13.casestudy.R

--- NYC.flights.13.casestudy.R
+++ NYC.flights.13.casestudy.R
@@ -6,22 +6,29 @@ library(dplyr)
 library(rmarkdown)
 library(ggplot2)
 library(nycflights13)
-#dim(flights)
-#by_origin <- group_by(flights,origin)   # groupby origin
-#delay <- summarise(by_origin,
-#                   count = n(),        # count the number of flights
-#                   dist = mean (distance, na.rm = TRUE),     # average distance 
-#                   delay = mean(arr_delay, na.rm = TRUE))    # na.rm = TRUE is not zero
-#delay <- filter(delay, delay >6)      # any flight delay is over 6
-#delay                                 # print delay
+dim(flights)
+by_origin <- group_by(flights,origin)   # groupby origin
+delay <- summarise(by_origin,
+                   count = n(),        # count the number of flights
+                   dist = mean (distance, na.rm = TRUE),     # average distance 
+                   delay = mean(arr_delay, na.rm = TRUE))    # na.rm = TRUE is not zero
+delay <- filter(delay, delay >6)      # any flight delay is over 6
+delay                                 # print delay
 
-delay_pipe <- flights %>%          # %>% is pipeline operator
-  filter(!is.na(dep_delay)) %>%    # ! means NOT, filter all data is not missing
-  group_by(origin) %>%             #  
-  summarise(
-    dis= mean(distance, na.rm = TRUE),
-    delay = mean(dep_delay),
-    count = n()) %>%
-    filter(count >0)
-delay_pipe
-  
+#delay_pipe <- flights %>%          # %>% is pipeline operator
+  #filter(!is.na(dep_delay)) %>%    # ! means NOT, filter all data is not missing
+  #group_by(origin) %>%             #  
+ # summarise(
+#    dis= mean(distance, na.rm = TRUE),
+   # delay = mean(dep_delay),
+  #  count = n()) %>%
+ #   filter(count >0)
+#delay_pipe
+
+library(ggplot2)
+ggplot(delay,aes(x = dist, y= delay))+     #x, y axis
+  geom_point(aes(color = count, size = count),alpha = 1/2)+  
+  xlab("Distance") +
+  ggtitle("Distance vs Delay")+
+  geom_smooth() +
+  scale_size_area()
